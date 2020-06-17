cbd57de9d7251793e770946f7fc335abeba28a36
theryancook <ryancook12@gmail.com>
2020-06-17 15:52:37 +1000

    add / remove all functionality

--- code.js
+++ code.js
@@ -1,7 +1,7 @@
 // First of all we need to create some variables where we can access the elements
-let ul = document.getElementById("list")
-let removeAll = document.getElementById("removeAll")
-let add = document.getElementById("add")
+let ul = document.getElementById("list");
+let removeAll = document.getElementById("removeAll");
+let add = document.getElementById("add");
 
 // Then we need to make some functions that manipulate the elements
 add.onclick = function () {
@@ -9,5 +9,17 @@ add.onclick = function () {
 }
 
 function addLi(targetUi) {
-    let inputText
+    let inputText = document.getElementById("text").value;
+    let li = document.createElement("li");
+    let textNode = document.createTextNode(inputText + " ");
+    let removeButton = document.createElement("button");
+    document.getElementById("text").value = " ";
+
+    if (inputText.length === 0) {
+        alert("Please add your to-dop item");
+        return false
+    }
+
+    removeButton.className = "removeMe";
+    removeButton.innerHTML = "Done!"
 }
