18f7b399f52dbeff643ca7e641b790bcb99167d5
theryancook <ryancook12@gmail.com>
2020-06-17 15:45:27 +1000

    added variables for the js

---
+++ code.js
@@ -0,0 +1,13 @@
+// First of all we need to create some variables where we can access the elements
+let ul = document.getElementById("list")
+let removeAll = document.getElementById("removeAll")
+let add = document.getElementById("add")
+
+// Then we need to make some functions that manipulate the elements
+add.onclick = function () {
+    addLi(ul);
+}
+
+function addLi(targetUi) {
+    let inputText
+}
